a0629daa17809a458e92ac0bd0a7861da9d31074
Vanshika100 <119647273+Vanshika100@users.noreply.github.com>
2022-12-14 17:23:46 +0530

    Delete greatest number.py

--- greatest number.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter number a:"))
-b=int(input("enter number b:"))
-c=int(input("enter number c:"))
-if a>b and a>c:
-    print('a is the greatest integer')
-elif b>a and b>c:
-    print('b is the greatest integer')
-else:
-    print('c is the greatest integer')
